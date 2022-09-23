be97c2bb56a3cdbbb7d8fc39b8d796f2fd31b466
nimbul <nimbuldev@gmail.com>
2022-09-23 02:38:28 -0500

    made robust

--- apis/wave.lua
+++ apis/wave.lua
@@ -1,67 +1,16 @@
---[[
-wave version 0.1.5
-
-The MIT License (MIT)
-Copyright (c) 2020 CrazedProgrammer
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-associated documentation files (the "Software"), to deal in the Software without restriction,
-including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
-so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or
-substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-]]
-
--- I'm so happy I don't have to write or understand this code, thanks! - ax.
-
-
 local wave = {}
-wave.version = "0.1.5"
+wave.version = "2.0.1"
+-- Modified to be slightly more robust against malformed song files
 
 wave._oldSoundMap = { "harp", "bassattack", "bd", "snare", "hat" }
-wave._newSoundMap = { "harp", "bass", "basedrum", "snare", "hat" }
+wave._newSoundMap = { "harp", "bass", "basedrum", "snare", "hat", "guitar", "flute", "bell", "chime", "xylophone" }
 wave._defaultThrottle = 99
 wave._defaultClipMode = 1
 wave._maxInterval = 1
-wave._isNewSystem = false
-if _HOST then
-	-- Redoing this, the correct and boring way, otherwise it doesn't work with versions above 1.100 until 1.800. - axisok
-	-- Likely to break only if CC:Tweaked changes how it writes versions.
-	local _matches = {}
-	for s in string.gmatch(_HOST, "%S+") do
-		_matches[#_matches + 1] = s
-	end
-
-	local _v = _matches[2]
-	_matches = {}
-	for s in string.gmatch(_v, "%P+") do
-		_matches[#_matches + 1] = s
-	end
-
-	local _new = { 1, 80 }
-	wave._isNewSystem = true
-	for i = 1, #_new, 1 do
-		if (i <= #_matches and _new[i] < tonumber(_matches[i])) then
-			break
-
-		elseif (i <= #_matches and _new[i] > tonumber(_matches[i])) then
-			wave._isNewSystem = false
-			break
-		end
-
-	end
-
-
-end
+wave._isNewSystem = true
+-- if _HOST then
+--	wave._isNewSystem = _HOST:sub(15, #_HOST) >= "1.80"
+-- end
 
 wave.context = {}
 wave.output = {}
@@ -75,7 +24,7 @@ function wave.createContext(clock, volume)
 	local context = setmetatable({}, { __index = wave.context })
 	context.outputs = {}
 	context.instances = {}
-	context.vs = { 0, 0, 0, 0, 0 }
+	context.vs = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 	context.prevClock = clock
 	context.volume = volume
 	return context
@@ -165,7 +114,7 @@ function wave.context:update(interval)
 	for i = 1, #self.outputs do
 		self.outputs[i].notes = 0
 	end
-	for i = 1, 5 do
+	for i = 1, 10 do
 		self.vs[i] = 0
 	end
 	if interval > 0 then
@@ -196,25 +145,14 @@ function wave.createOutput(out, volume, filter, throttle, clipMode)
 		output.type = "custom"
 		return output
 	elseif type(out) == "string" then
-		if peripheral.getType(out) == "iron_noteblock" then
-			if wave._isNewSystem then
-				local nb = peripheral.wrap(out)
-				output.type = "iron_noteblock"
-				function output.nativePlayNote(note, pitch, vol)
-					if output.volume * vol > 0 then
-						nb.playSound("minecraft:block.note." .. wave._newSoundMap[note], vol, math.pow(2, (pitch - 12) / 12))
-					end
-				end
-
-				return output
-			end
-		elseif peripheral.getType(out) == "speaker" then
+		if peripheral.getType(out) == "speaker" then
 			if wave._isNewSystem then
 				local nb = peripheral.wrap(out)
 				output.type = "speaker"
-				function output.nativePlayNote(note, pitch, vol)
-					if output.volume * vol > 0 then
-						nb.playNote(wave._newSoundMap[note], vol, pitch)
+				function output.nativePlayNote(note, pitch, volume)
+					if output.volume * volume > 0 then
+						--nb.playSound("minecraft:block.note_block."..wave._newSoundMap[note], volume, math.pow(2, (pitch - 12) / 12))
+						nb.playNote(wave._newSoundMap[note], volume, pitch)
 					end
 				end
 
@@ -225,14 +163,16 @@ function wave.createOutput(out, volume, filter, throttle, clipMode)
 		if out.execAsync then
 			output.type = "commands"
 			if wave._isNewSystem then
-				function output.nativePlayNote(note, pitch, vol)
-					out.execAsync("playsound minecraft:block.note." ..
-						wave._newSoundMap[note] .. " record @a ~ ~ ~ " .. tostring(vol) .. " " .. tostring(math.pow(2, (pitch - 12) / 12)))
+				function output.nativePlayNote(note, pitch, volume)
+					out.execAsync("playsound minecraft:block.note_block." ..
+						wave._newSoundMap[note] ..
+						" record @a ~ ~ ~ " .. tostring(volume) .. " " .. tostring(math.pow(2, (pitch - 12) / 12)))
 				end
 			else
-				function output.nativePlayNote(note, pitch, vol)
-					out.execAsync("playsound note." ..
-						wave._oldSoundMap[note] .. " @a ~ ~ ~ " .. tostring(vol) .. " " .. tostring(math.pow(2, (pitch - 12) / 12)))
+				function output.nativePlayNote(note, pitch, volume)
+					out.execAsync("playsound note_block." ..
+						wave._oldSoundMap[note] ..
+						" @a ~ ~ ~ " .. tostring(volume) .. " " .. tostring(math.pow(2, (pitch - 12) / 12)))
 				end
 			end
 			return output
@@ -251,9 +191,7 @@ function wave.scanOutputs()
 	end
 	local sides = peripheral.getNames()
 	for i = 1, #sides do
-		if peripheral.getType(sides[i]) == "iron_noteblock" then
-			outs[#outs + 1] = wave.createOutput(sides[i])
-		elseif peripheral.getType(sides[i]) == "speaker" then
+		if peripheral.getType(sides[i]) == "speaker" then
 			outs[#outs + 1] = wave.createOutput(sides[i])
 		end
 	end
@@ -286,6 +224,117 @@ function wave.output:playNote(note, pitch, volume)
 	end
 end
 
+function wave.loadNewTrack(path)
+
+	local track = setmetatable({}, { __index = wave.track })
+	local handle = fs.open(path, "rb")
+	if not handle then return end
+
+	local function readInt(size)
+		local num = 0
+		for i = 0, size - 1 do
+			local byte = handle.read()
+			if not byte then -- dont leave open file handles no matter what
+				handle.close()
+				return
+			end
+			num = num + byte * (256 ^ i)
+		end
+		return num
+	end
+
+	local function readStr()
+		local length = readInt(4)
+		if not length then return end
+		local data = {}
+		for i = 1, length do
+			data[i] = string.char(handle.read())
+		end
+		return table.concat(data)
+	end
+
+	-- Part #1: Metadata
+	print("New format bytes: " .. readInt(2))
+
+	track.version = readInt(1);
+	track.instCount = readInt(1);
+	track.length = readInt(2) -- song length (ticks)
+	track.height = readInt(2) -- song height
+	track.name = readStr() -- song name
+	track.author = readStr() -- song author
+	track.originalAuthor = readStr() -- original song author
+	track.description = readStr() -- song description
+	track.tempo = readInt(2) / 100 -- tempo (ticks per second)
+	track.autoSaving = readInt(1) == 0 and true or false -- auto-saving
+	track.autoSavingDuration = readInt(1) -- auto-saving duration
+	track.timeSignature = readInt(1) -- time signature (3 = 3/4)
+	track.minutesSpent = readInt(4) -- minutes spent
+	track.leftClicks = readInt(4) -- left clicks
+	track.rightClicks = readInt(4) -- right clicks
+	track.blocksAdded = readInt(4) -- blocks added
+	track.blocksRemoved = readInt(4) -- blocks removed
+	track.schematicFileName = readStr() -- midi/schematic file name
+	track.loop = readInt(1)
+	track.maxLoopCount = readInt(1)
+	track.loopStartTick = readInt(2)
+
+
+
+
+
+	-- Part #2: Notes
+	track.layers = {}
+	for i = 1, track.height do
+		track.layers[i] = { name = "Layer " .. i, volume = 1.0 }
+		track.layers[i].notes = {}
+	end
+
+	local tick = 0
+	while true do
+		local tickJumps = readInt(2)
+		if tickJumps == 0 then break end
+		tick = tick + tickJumps
+		local layer = 0
+		while true do
+			local layerJumps = readInt(2)
+			if layerJumps == 0 then break end
+			layer = layer + layerJumps
+			if layer > track.height then -- nbs can be buggy
+				for i = track.height + 1, layer do
+					track.layers[i] = { name = "Layer " .. i, volume = 1.0 }
+					track.layers[i].notes = {}
+				end
+				track.height = layer
+			end
+			local instrument = readInt(1)
+			local key = readInt(1)
+			local noteBlockVolume = readInt(1)
+			local noteBlockPan = readInt(1)
+			local noteBlockPitch = readInt(2)
+			if instrument <= 9 then -- nbs can be buggy
+				track.layers[layer].notes[tick * 2 - 1] = instrument + 1
+				track.layers[layer].notes[tick * 2] = key - 33
+			end
+		end
+	end
+
+
+	-- Part #3: Layers
+	for i = 1, track.height do
+		local name = readStr()
+		local layerLock = readInt(1)
+		local layerVolume = readInt(1)
+		local layerStereo = readInt(1)
+		if not name then print("NO NAME") break end -- if layer data doesnt exist, abort
+		track.layers[i].name = name
+		track.layers[i].volume = layerVolume / 100
+	end
+
+
+	handle.close()
+	return track
+end
+
 function wave.loadTrack(path)
 	local track = setmetatable({}, { __index = wave.track })
 	local handle = fs.open(path, "rb")
@@ -315,7 +364,15 @@ function wave.loadTrack(path)
 	end
 
 	-- Part #1: Metadata
-	track.length = readInt(2) -- song length (ticks)
+
+	firstBytes = readInt(2)
+	if firstBytes == 0 then
+		print("Found new NBS file; Using new loader...")
+		handle.close()
+		return wave.loadNewTrack(path)
+	end
+
+	track.length = firstBytes -- song length (ticks)
 	track.height = readInt(2) -- song height
 	track.name = readStr() -- song name
 	track.author = readStr() -- song author
@@ -358,7 +415,7 @@ function wave.loadTrack(path)
 			end
 			local instrument = readInt(1)
 			local key = readInt(1)
-			if instrument <= 4 then -- nbs can be buggy
+			if instrument <= 9 then -- nbs can be buggy
 				track.layers[layer].notes[tick * 2 - 1] = instrument + 1
 				track.layers[layer].notes[tick * 2] = key - 33
 			end
@@ -373,7 +430,7 @@ function wave.loadTrack(path)
 		track.layers[i].volume = readInt(1) / 100
 	end
 
-	-- handle.close() The API sometimes crashes unless I comment this out, probably causes memory leaks
+	pcall(handle.close)
 	return track
 end
 
